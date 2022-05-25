62c2eeebe0d6eb9d791c63d6baf34379b7ae9cc7
timikos <timikosbison@gmail.com>
2022-05-25 20:57:15 +0300

    add Tabs

--- src/components/tabs-price/tabs-price.jsx
+++ src/components/tabs-price/tabs-price.jsx
@@ -1,23 +1,58 @@
-const TabsPrice = () => {
-  // const [activeBtn, setActiveBtn] = useState(1)
-  // const tabs = [1, 2, 3]
-  // const names = ['САМЫЙ ДЕШЕВЫЙ', 'САМЫЙ БЫСТРЫЙ', 'ОПТИМАЛЬНЫЙ']
+import './tabs-price.scss'
 
-  // const elements = tabs.map((elem, index) => {
-  //   return (
-  //     <button
-  //       key={index}
-  //       value={index}
-  //       onClick={activateButton}
-  //       className={activeBtn === index ? 'btn-tab_active' : ''}
-  //     >
-  //       {names[index]}
-  //     </button>
-  //   )
-  // })
+const TabsPrice = () => {
+  // const [value, setValue] = useState('1')
+  //
+  // const handleChange = (event, newValue) => {
+  //   setValue(newValue)
+  // }
 
   return (
-    <div />
+    <div className="tabs__container">
+      <div className="btn-group" role="group" aria-label="Basic">
+        <input
+          type="radio"
+          className="btn-check"
+          name="btnradio"
+          id="btnradio1"
+          autoComplete="off"
+          checked=""
+        />
+        <label
+          className="btn btn-outline-primary tab__btn"
+          htmlFor="btnradio1"
+        >
+          САМЫЙ ДЕШЕВЫЙ
+        </label>
+        <input
+          type="radio"
+          className="btn-check"
+          name="btnradio"
+          id="btnradio2"
+          autoComplete="off"
+        />
+        <label
+          className="btn btn-outline-primary tab__btn"
+          htmlFor="btnradio2"
+        >
+          САМЫЙ БЫСТРЫЙ
+        </label>
+
+        <input
+          type="radio"
+          className="btn-check"
+          name="btnradio"
+          id="btnradio3"
+          autoComplete="off"
+        />
+        <label
+          className="btn btn-outline-primary tab__btn"
+          htmlFor="btnradio3"
+        >
+          ОПТИМАЛЬНЫЙ
+        </label>
+      </div>
+    </div>
   )
 }
 
