d3f628d2f118ccc37af8235a44d19bd6c3a87a04
timikos <timikosbison@gmail.com>
2022-05-30 21:10:24 +0300

    added working filters

--- src/containers/Filters/Filters.jsx
+++ src/containers/Filters/Filters.jsx
@@ -7,88 +7,71 @@ import * as checkboxActions from '../../redux/actions/checkbox'
 
 const Filters = () => {
   const state = useSelector(state => ({
-    activeCheckbox: state.checkboxReducer.activeCheckbox
+    filters: state.checkboxReducer
   }))
   const dispatch = useDispatch()
-  const optionsCheckbox = [
-    {
-      name: 'Все',
-      active: true,
-      onClick: () => {
-        dispatch(checkboxActions.setCheckboxAll())
-      }
-    },
-    {
-      name: 'Без пересадок',
-      active: false,
-      onClick: () => {
-        dispatch(checkboxActions.setCheckboxWithout())
-      }
-    },
-    {
-      name: '1 пересадка',
-      active: false,
-      onClick: () => {
-        dispatch(checkboxActions.setCheckboxOne())
-      }
-    },
-    {
-      name: '2 пересадки',
-      active: true,
-      onClick: () => {
-        dispatch(checkboxActions.setCheckboxTwo())
-      }
-    },
-    {
-      name: '3 пересадки',
-      active: false,
-      onClick: () => {
-        dispatch(checkboxActions.setCheckboxThree())
-      }
-    },
-  ]
 
-  const handle = element => {
-    element.active = !element.active
-  }
-
-
-  const filters = Object.values(optionsCheckbox).map((elem, index) => {
-    return (
-      <FormControlLabel
-        checked={elem.active}
-        key={index}
-        onChange={() => handle(elem)}
-        className="filter__checkbox"
-        control={<Checkbox />}
-        label={elem.name}
-        sx={{
-          ml: 0,
-          mr: 0,
-        }}
-      />
-    )
-  })
-
-  if (state.activeCheckbox === 'all') {
-    optionsCheckbox[0].active = true
-    optionsCheckbox[1].active = true
-    optionsCheckbox[2].active = true
-    optionsCheckbox[3].active = true
-    optionsCheckbox[4].active = true
-  }
-  if (state.activeCheckbox !== 'all') {
-    optionsCheckbox[0].active = false
-  }
-
-
-  console.log(state)
-  console.log(optionsCheckbox)
   return (
     <div className="filter__container">
       <label className="filter__label">КОЛИЧЕСТВО ПЕРЕСАДОК</label>
       <FormGroup className="filter__checkbox-group">
-        {filters}
+        <FormControlLabel
+          checked={state.filters.all}
+          onChange={() => {
+            dispatch(checkboxActions.setCheckboxAll())
+          }}
+          className="filter__checkbox"
+          control={<Checkbox />}
+          label="Все"
+          sx={{
+            ml: 0,
+            mr: 0,
+          }}
+        />
+        <FormControlLabel
+          checked={state.filters.without}
+          onChange={() => dispatch(checkboxActions.setCheckboxWithout())}
+          className="filter__checkbox"
+          control={<Checkbox />}
+          label="Без пересадок"
+          sx={{
+            ml: 0,
+            mr: 0,
+          }}
+        />
+        <FormControlLabel
+          checked={state.filters.one}
+          onChange={() => dispatch(checkboxActions.setCheckboxOne())}
+          className="filter__checkbox"
+          control={<Checkbox />}
+          label="1 пересадка"
+          sx={{
+            ml: 0,
+            mr: 0,
+          }}
+        />
+        <FormControlLabel
+          checked={state.filters.two}
+          onChange={() => dispatch(checkboxActions.setCheckboxTwo())}
+          className="filter__checkbox"
+          control={<Checkbox />}
+          label="2 пересадки"
+          sx={{
+            ml: 0,
+            mr: 0,
+          }}
+        />
+        <FormControlLabel
+          checked={state.filters.three}
+          onChange={() => dispatch(checkboxActions.setCheckboxThree())}
+          className="filter__checkbox"
+          control={<Checkbox />}
+          label="3 пересадки"
+          sx={{
+            ml: 0,
+            mr: 0,
+          }}
+        />
       </FormGroup>
     </div>
   )
--- src/redux/reducers/checkboxReducer.js
+++ src/redux/reducers/checkboxReducer.js
@@ -8,32 +8,87 @@ import {
 } from '../actions/actionTypes'
 
 const initialState = {
-  activeCheckbox: '',
-  activeTabPrice: '',
+  all: false,
+  without: false,
+  one: false,
+  two: false,
+  three: false,
 }
 
 const checkboxReducer = (state = initialState, action) => {
-  console.log(action.type)
   switch (action.type) {
   case ACTIVE_CHECKBOX_ALL:
+    if (state.all === false) {
+      return {
+        all: true,
+        without: true,
+        one: true,
+        two: true,
+        three: true
+      }
+    }
     return {
-      ...state, activeCheckbox: 'all'
+      all: !state.all,
+      without: !state.without,
+      one: !state.one,
+      two: !state.two,
+      three: !state.three
     }
   case ACTIVE_CHECKBOX_WITHOUT:
+    if (state.all === false) {
+      if (state.one && state.two && state.three) {
+        return {
+          ...state, without: !state.without, all: !state.all
+        }
+      }
+      return {
+        ...state, without: !state.without
+      }
+    }
     return {
-      ...state, activeCheckbox: 'without'
+      ...state, all: !state.all, without: !state.without
     }
   case ACTIVE_CHECKBOX_ONE:
+    if (state.all === false) {
+      if (state.without && state.two && state.three) {
+        return {
+          ...state, one: !state.one, all: !state.all
+        }
+      }
+      return {
+        ...state, one: !state.one
+      }
+    }
     return {
-      ...state, activeCheckbox: 'one'
+      ...state, all: !state.all, one: !state.one
     }
   case ACTIVE_CHECKBOX_TWO:
+    if (state.all === false) {
+      if (state.one && state.without && state.three) {
+        return {
+          ...state, two: !state.two, all: !state.all
+        }
+      }
+      return {
+        ...state, two: !state.two
+      }
+    }
     return {
-      ...state, activeCheckbox: 'two'
+      ...state, all: !state.all, two: !state.two
     }
   case ACTIVE_CHECKBOX_THREE:
+    if (state.all === false) {
+      if (state.one && state.two && state.without) {
+        return {
+          ...state, three: !state.three, all: !state.all
+        }
+      }
+      return {
+        ...state, three: !state.three
+      }
+    }
     return {
-      ...state, activeCheckbox: 'three'
+      ...state, all: !state.all, three: !state.three
     }
   case ACTIVE_TAB_PRICE:
     return {
